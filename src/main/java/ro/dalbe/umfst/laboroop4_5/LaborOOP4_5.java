d4bc9c2c2068d559bf89bd1d3aebf687ac628858
torok <torok@DalbeLenovoT15>
2025-10-08 11:45:34 +0300

    Project created

---
+++ src/main/java/ro/dalbe/umfst/laboroop4_5/LaborOOP4_5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ro.dalbe.umfst.laboroop4_5;
+
+/**
+ *
+ * @author torok
+ */
+public class LaborOOP4_5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
